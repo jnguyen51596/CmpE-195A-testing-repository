9e88191dfa8024598e7a237aee6db7acd598f371
mmilanovich <mmilanovich@gmail.com>
2015-05-17 13:44:03 -0700

    more small changes

--- Actions/executeLoginCheck.php
+++ Actions/executeLoginCheck.php
@@ -1,11 +1,13 @@
 <?php
+session_start();
+
 $sjsuid = $_POST['name'];
 $password =$_POST['pwd'];
 require 'pdoj.php';
 checkLogin($sjsuid, $password);
 if(checkLogin($sjsuid, $password)==true)
 {
-    session_start();
+    
     $_SESSION["username"]= $sjsuid;
     echo "true";
 }
--- Actions/getStudentCourses.php
+++ Actions/getStudentCourses.php
@@ -1,7 +1,9 @@
 <?php
+	session_start();
 	include "sql/pdo.php";
 
 	// TODO change this to session variable once implemented
+	//$studentID = $_SESSION['username'];
 	$studentID = 1;
 	$result = getStudentCourses($studentID);
 	header('Content-Type: application/json');	
