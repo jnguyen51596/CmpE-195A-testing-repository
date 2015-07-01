3b93e7b77ac1f1a4931b93493079465470c103b7
mmilanovich <mmilanovich@gmail.com>
2015-06-30 20:33:53 -0700

    removed login.js

--- Responders/createQuiz.php
+++ Responders/createQuiz.php
@@ -14,6 +14,7 @@
         <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
         <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 		-->
+
 		<link rel="stylesheet" href="../css/font-awesome.min.css" />
 		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
 		<link rel="stylesheet" href="../css/jquerymobile.nativedroid.css" />
@@ -23,7 +24,6 @@
 		<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
 		<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 
-        <script src="../Actions/login.js"></script>
         <script src="../Actions/quiz.js"></script>
         <script src="../Actions/javascriptFunction.js"></script>
 
