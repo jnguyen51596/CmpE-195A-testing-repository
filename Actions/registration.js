6dd447bc3719dab69a34558891fed968b83a78c6
cpc1992 <cliffordpchan@gmail.com>
2015-06-30 20:34:40 -0700

    updated old stuff
    derp

---
+++ Actions/registration.js
@@ -0,0 +1,41 @@
+function checkinfo() {
+                
+               
+                var username = document.getElementById("username").value;
+                var first = document.getElementById("firstname").value;
+                var last = document.getElementById("lastname").value;
+                var pass1 = document.getElementById("password").value;
+                var pass2 = document.getElementById("confirm").value;
+                //alert(username);
+                if(username === "" || first === "" || last === "" || pass1 === "" || pass2 === "")
+                {
+                    alert("You left one or more fields empty. Please fill out everything.");
+                    
+                    //DO ANOTHER ELSEIF AND CHECK USERNAME AGAINST THE DB AFTER THIS ONE
+                }else if(pass1 !== pass2)
+                {
+                    alert("Your passwords are not matching. Fix that!");
+                }else
+                {
+
+                        $.ajax({
+                            type: "POST",
+                            url: "../Actions/register.php",
+                            data: "username=" + username + "&first=" + first + "&last=" + last + "&pass1=" + pass1 + "&pass2=" + pass2,
+                            cache: false,
+                            success: function (data) {
+                                if (data == 1) {
+                                    alert("Successful Registration!!!");
+                                    window.location = 'userHome.php';
+
+                                }
+                                else {
+                                    alert('db crashed bro');
+                                }
+                            }
+                        });
+                    
+                }
+                                      
+
+            }
--- Responders/registration.php
+++ Responders/registration.php
@@ -25,51 +25,8 @@ and open the template in the editor.
 				
 		<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
 		<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-	
+                <script src="../Actions/registration.js"></script>
 		
-        <script>
-            function checkinfo() {
-                
-               
-                var username = document.getElementById("username").value;
-                var first = document.getElementById("firstname").value;
-                var last = document.getElementById("lastname").value;
-                var pass1 = document.getElementById("password").value;
-                var pass2 = document.getElementById("confirm").value;
-                //alert(username);
-                if(username === "" || first === "" || last === "" || pass1 === "" || pass2 === "")
-                {
-                    alert("You left one or more fields empty. Please fill out everything.");
-                    
-                    //DO ANOTHER ELSEIF AND CHECK USERNAME AGAINST THE DB AFTER THIS ONE
-                }else if(pass1 !== pass2)
-                {
-                    alert("Your passwords are not matching. Fix that!");
-                }else
-                {
-
-                        $.ajax({
-                            type: "POST",
-                            url: "../Actions/register.php",
-                            data: "username=" + username + "&first=" + first + "&last=" + last + "&pass1=" + pass1 + "&pass2=" + pass2,
-                            cache: false,
-                            success: function (data) {
-                                if (data == 1) {
-                                    alert("Successful Registration!!!");
-                                    window.location = 'userHome.php';
-
-                                }
-                                else {
-                                    alert('db crashed bro');
-                                }
-                            }
-                        });
-                    
-                }
-                                      
-
-            }
-        </script>
     </head>
     <body>
 	<div data-role="page" data-theme="b">
