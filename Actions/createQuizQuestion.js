6ac0d76378912bb9d7ab5679fd8f116c6f397446
jnguyen51596 <icare202@gmail.com>
2015-12-05 01:56:44 -0800

    commented short answer

--- Actions/createQuizQuestion.js
+++ Actions/createQuizQuestion.js
@@ -2,21 +2,21 @@ function showA()
 {
     document.getElementById("box").style.display = "initial";
     document.getElementById("box2").style.display = "none";
-    document.getElementById("box3").style.display = "none";
+//    document.getElementById("box3").style.display = "none";
 }
 function showB()
 {
     document.getElementById("box").style.display = "none";
     document.getElementById("box2").style.display = "initial";
-    document.getElementById("box3").style.display = "none";
+//    document.getElementById("box3").style.display = "none";
 }
 
-function showC()
-{
-    document.getElementById("box").style.display = "none";
-    document.getElementById("box2").style.display = "none";
-    document.getElementById("box3").style.display = "initial";
-}
+//function showC()
+//{
+//    document.getElementById("box").style.display = "none";
+//    document.getElementById("box2").style.display = "none";
+//    document.getElementById("box3").style.display = "initial";
+//}
 
 function submitQuiz()
 {
@@ -63,7 +63,7 @@ function submitQuiz()
             });
         }
     }
-    else if (document.getElementById("radio-choice-h-2b").checked)
+    else
     {
         questionType = $("#radio-choice-h-2b").val();
         question = $('#question-b').val();
@@ -102,34 +102,34 @@ function submitQuiz()
         }
 
     }
-    else
-    {
-        questionType = $("#radio-choice-h-2c").val();
-        question = $('#question-c').val();
-        if (quiznumber == "" || question == "")
-        {
-            alert("Incorrect Submission");
-        }
-        else
-        {
-            $.ajax({
-                type: "POST",
-                url: "/Actions/executeQuizSubmit.php",
-                data: "questionType=" + questionType + "&classID=" + classID + "&quiznumber=" + quiznumber + "&question=" + question,
-                cache: false,
-                success: function (data) {
-                    if (data == true)
-                    {
-                        alert("Correct Submission");
-                        window.location = '/home/instructor-home/manage-quizzes/quiz-end/';
-                    }
-                    else
-                    {
-                        alert("Quiz Question already submitted");
-                    }
-                }
-            });
-        }
-    }
+//    else
+//    {
+//        questionType = $("#radio-choice-h-2c").val();
+//        question = $('#question-c').val();
+//        if (quiznumber == "" || question == "")
+//        {
+//            alert("Incorrect Submission");
+//        }
+//        else
+//        {
+//            $.ajax({
+//                type: "POST",
+//                url: "/Actions/executeQuizSubmit.php",
+//                data: "questionType=" + questionType + "&classID=" + classID + "&quiznumber=" + quiznumber + "&question=" + question,
+//                cache: false,
+//                success: function (data) {
+//                    if (data == true)
+//                    {
+//                        alert("Correct Submission");
+//                        window.location = '/home/instructor-home/manage-quizzes/quiz-end/';
+//                    }
+//                    else
+//                    {
+//                        alert("Quiz Question already submitted");
+//                    }
+//                }
+//            });
+//        }
+//    }
 
 }
