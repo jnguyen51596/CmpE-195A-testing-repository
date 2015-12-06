d045331a0ff0417de3a1d72b27bd18f0343a42b7
jnguyen51596 <icare202@gmail.com>
2015-12-05 19:51:34 -0800

    added checks to asssignments

--- Actions/createAssignment.js
+++ Actions/createAssignment.js
@@ -11,28 +11,36 @@ function createAssignment() {
 	// format datepicker here.
 	var description = document.getElementById("desc-id").value;
 	// var ampm = document.getElementById("ampm-id").value; // more sophisticated time options to be added...
-	
-    $.ajax({
-		type: "POST",
-		url: "/Actions/createAssignment.php",
-        data: {courseID: courseID, title: title, total: total, duedate: duedate, description: description}, 
-        dataType: "json",
-		success: function(data) {
-			alert("Assignment created!");
-			window.location.href = "/home/instructor-home";
-			// var successTag = '<div class="message success">' +
-			// '<i class="fa fa-check"></i>' +
-			// '<p>Assignment created!</p>' +
-			// '</div>';
-			// $('#status-id').append(successTag);
-		},
-		//error: function(xhr, ajaxOptions, thrownError) { alert("qerror: " + data + xhr.responseText + " " + ajaxOptions + " " + thrownError); }
-		error: function(data) { console.log(data); }
-	});
-	document.getElementById("assignmentname-id").value = "";
-	document.getElementById("points-id").value = "";
-	document.getElementById("time-id").value = "";
-	document.getElementById("desc-id").value = "";
+ if (description=="" || title=="" || total =="" || duedate=="")
+    {
+        alert("Please fill in all fields");
+    }
+    else
+    {
+        $.ajax({
+            type: "POST",
+            url: "/Actions/createAssignment.php",
+            data: {courseID: courseID, title: title, total: total, duedate: duedate, description: description},
+            dataType: "json",
+            success: function (data) {
+                alert("Assignment created!");
+                window.location.href = "/home/instructor-home";
+                // var successTag = '<div class="message success">' +
+                // '<i class="fa fa-check"></i>' +
+                // '<p>Assignment created!</p>' +
+                // '</div>';
+                // $('#status-id').append(successTag);
+            },
+            //error: function(xhr, ajaxOptions, thrownError) { alert("qerror: " + data + xhr.responseText + " " + ajaxOptions + " " + thrownError); }
+            error: function (data) {
+                console.log(data);
+            }
+        });
+        document.getElementById("assignmentname-id").value = "";
+        document.getElementById("points-id").value = "";
+        document.getElementById("time-id").value = "";
+        document.getElementById("desc-id").value = "";
+    }
 }
 
 function getRemote(instructor) {
