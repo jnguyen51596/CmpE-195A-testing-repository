6ec7c054f7b25c20ad69c583ccdb93ee6c015623
mmilanovich <mmilanovich@gmail.com>
2015-12-05 20:41:19 -0800

    left in debug statement

--- Actions/getQuizzesToTake.php
+++ Actions/getQuizzesToTake.php
@@ -4,9 +4,6 @@ require 'sql/pdo.php';
 
 $class = $_POST['classid'];
 $userID = $_SESSION['userID'];
-$class = 7;
-$userID = 18;
-
 
 $rows = getQuizzesToTake($class, $userID);
 if (count($rows) == 0) {
